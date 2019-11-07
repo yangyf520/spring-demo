3e2fc2943008dcedd12d7304fb67ef8fa277b0dc
杨雨峰 <yyang@cccis.com>
2019-11-07 22:16:20 +0800

    添加MyBatis事物

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-demo
+
+需要把建表的Engine设置为InnoDB格式，
+自动建表的格式为MyISAM不支持事务管理。
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 
+@EnableTransactionManagement
 @SpringBootApplication
-@MapperScan("com.example.demo.*.mapper") //事物不填该注解
+@MapperScan("com.example.*.mapper") //事物不填该注解
 public class DemoApplication {
 
     public static void main(String[] args) {
